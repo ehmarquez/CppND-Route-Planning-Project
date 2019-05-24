0185c25539a81c85180ecda9e7ea072afb732d0b
Alvin Marquez <alvinmarquez7@gmail.com>
2019-05-23 21:16:09 -0400

    implementation of RouteModel constructor to create RouteModel Nodesgs

--- src/route_model.cpp
+++ src/route_model.cpp
@@ -2,5 +2,10 @@
 #include <iostream>
 
 RouteModel::RouteModel(const std::vector<std::byte> &xml) : Model(xml) {
-
+  // Creating RouteModel Nodes
+  int iter = 0;
+  for(Model::Node node : this->Nodes()) {
+    m_Nodes.push_back(Node(iter, this, node));
+    iter++;
+  }
 }

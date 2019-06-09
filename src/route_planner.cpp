2c231d4a4b017744872cf107a58c776b58a3a426
Alvin Marquez <alvinmarquez7@gmail.com>
2019-06-08 23:13:21 -0400

    implementation of RoutePlanner constructor

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -2,5 +2,13 @@
 #include <algorithm>
 
 RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y): m_Model(model) {
+  // Scale to percentage values
+  start_x *= 0.01;
+  start_y *= 0.01;
+  end_x *= 0.01;
+  end_y *= 0.01;
 
+  // Find closest nodes to start and end points
+  start_node = &model.FindClosestNode(start_x, start_y);
+  end_node = &model.FindClosestNode(end_x, end_y);
 }
--- src/route_planner.h
+++ src/route_planner.h
@@ -10,8 +10,15 @@ class RoutePlanner {
   public:
     RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y);
     // Add public variables or methods declarations here.
+    auto &GetDistance() const { return distance; }
 
   private:
     // Add private variables or methods declarations here.
     RouteModel &m_Model;
+    RouteModel::Node* start_node;          // nodes closest to start/end points
+    RouteModel::Node* end_node;
+    float distance;                       // from start_node -> end_node
+
+    // @return vector<RouteModel::Node>
+    auto ConstructFinalPath(RouteModel::Node* current_node);
 };
